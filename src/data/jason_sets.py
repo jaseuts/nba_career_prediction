58f6dadb7dbb6a69ca2df79b740e65d0dc1fe47d
jason <Thang.D.Nguyen@student.uts.edu.au>
2022-02-14 22:12:35 +1100

    jason resolved problem of pull request and reinstated stashed works shannon rebase

--- src/data/jason_sets.py
+++ src/data/jason_sets.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 def save_sets(X_train=None, y_train=None, X_val=None, y_val=None, X_test=None, y_test=None, path='../data/processed/'):
+=======
+def save_sets(X_train=None, y_train=None, X_val=None, y_val=None, X_test=None, y_test=None, test_id=None, path='../data/processed/'):
+>>>>>>> c97e6a1 (jason resolved problem of pull request and reinstated stashed works)
     """Save the different sets locally
 
     Parameters
@@ -15,6 +19,11 @@ def save_sets(X_train=None, y_train=None, X_val=None, y_val=None, X_test=None, y
         Features for the testing set
     y_test: Numpy Array
         Target for the testing set
+<<<<<<< HEAD
+=======
+    test_id: Numpy Array
+        The ID of instances in the testing set
+>>>>>>> c97e6a1 (jason resolved problem of pull request and reinstated stashed works)
     path : str
         Path to the folder where the sets will be saved (default: '../data/processed/')
 
@@ -35,11 +44,20 @@ def save_sets(X_train=None, y_train=None, X_val=None, y_val=None, X_test=None, y
       np.save(f'{path}y_val',   y_val)
     if y_test is not None:
       np.save(f'{path}y_test',  y_test)
+<<<<<<< HEAD
     
     
 
 def load_sets(path='../data/processed/', val=False):
     """Load the different locally save sets
+=======
+    if test_id is not None:
+      np.save(f'{path}test_id',  test_id)
+      
+      
+def load_sets(path='../data/processed/', val=False):
+    """Load various locally saved sets
+>>>>>>> c97e6a1 (jason resolved problem of pull request and reinstated stashed works)
 
     Parameters
     ----------
@@ -60,6 +78,11 @@ def load_sets(path='../data/processed/', val=False):
         Features for the testing set
     Numpy Array
         Target for the testing set
+<<<<<<< HEAD
+=======
+    Numpy Array
+        Row Id for the testing set      
+>>>>>>> c97e6a1 (jason resolved problem of pull request and reinstated stashed works)
     """
     import numpy as np
     import os.path
@@ -70,5 +93,12 @@ def load_sets(path='../data/processed/', val=False):
     y_train = np.load(f'{path}y_train.npy') if os.path.isfile(f'{path}y_train.npy') else None
     y_val   = np.load(f'{path}y_val.npy'  ) if os.path.isfile(f'{path}y_val.npy')   else None
     y_test  = np.load(f'{path}y_test.npy' ) if os.path.isfile(f'{path}y_test.npy')  else None
+<<<<<<< HEAD
     
     return X_train, y_train, X_val, y_val, X_test, y_test
+=======
+    test_id = np.load(f'{path}test_id.npy') if os.path.isfile(f'{path}test_id.npy') else None
+    
+    return X_train, y_train, X_val, y_val, X_test, y_test, test_id
+
+>>>>>>> c97e6a1 (jason resolved problem of pull request and reinstated stashed works)
